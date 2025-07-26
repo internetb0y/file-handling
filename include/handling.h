f5a1c6b3bad7d56cf3dc071734d5ba5beef0778d
internetb0y <1.internetboy.0@gmail.com>
2025-07-26 23:36:55 +0700

    fix: Modification main.c, Append new file handling.c and handling.h and create new executable file.

---
+++ include/handling.h
@@ -0,0 +1,4 @@
+#ifndef _HANDLING_H_
+#define _HANDLING_H_
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "./handling.c"
+
 int main(void) {
     printf("Hello World! \n");
 }
